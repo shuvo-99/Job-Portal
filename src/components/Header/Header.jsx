d5b1354d357fee49c4e6960996560332035995ce
shuvo-99 <anontoshuvo99@gmail.com>
2023-12-09 03:15:53 +0600

    header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,24 @@
 // import React from "react";
+import { Link } from "react-router-dom";
 import "./Header.css";
 
 const Header = () => {
   return (
-    <div>
-      <h5>Header</h5>
+    <div className="header m-5">
+      <div>
+        <Link to="/">Job Portal</Link>
+      </div>
+      <div className=" me-5">
+        <Link to="/explore_jobs">Explore Jobs</Link>
+        <Link to="/applied_jobs">Applied Jobs</Link>
+      </div>
+      <div>
+        <Link to="/Login">
+          <button className="bg-blue-500 text-white py-2 px-4 rounded">
+            Login
+          </button>
+        </Link>
+      </div>
     </div>
   );
 };
