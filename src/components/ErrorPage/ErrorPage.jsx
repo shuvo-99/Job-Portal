3ceaa4d4253d03e978319d1c0177f398eb9ce4c7
shuvo-99 <anontoshuvo99@gmail.com>
2023-12-10 02:51:52 +0600

    finishing

--- src/components/ErrorPage/ErrorPage.jsx
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -1,14 +1,32 @@
 // import React from "react";
 import { useRouteError } from "react-router-dom";
+import Header from "../Header/Header";
+import "./ErrorPage.css";
 
 const ErrorPage = () => {
   const error = useRouteError();
   console.log(error);
 
+  const headerColor = "#f1a5a5";
+
+  const containerStyle = {
+    backgroundColor: "#f1a5a5",
+  };
+
   return (
     <div id="error-page">
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
+      <Header headerColor={headerColor}></Header>
+      <section
+        style={containerStyle}
+        className=" text-center py-20 text-3xl font-bold"
+      >
+        <p>Not Found Page</p>
+      </section>
+      <div className="error">
+        <p>Its ERROR: 404</p>
+        <p>The URL maybe wrong.</p>
+        <p>Try again with the correct URL</p>
+      </div>
       {error && (
         <p>
           <i>{error.statusText || error.message}</i>
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,10 +1,26 @@
+import Header from "../Header/Header";
 import "./Login.css";
 
 const Login = () => {
+  const headerColor = "#b0ef80";
+
+  const containerStyle = {
+    backgroundColor: "#b0ef80",
+  };
   return (
-    <div className=" text-center">
-      <h1>Work on Progress</h1>
-      <h2>Stay stuned!!</h2>
+    <div>
+      <Header headerColor={headerColor}></Header>
+      <section
+        style={containerStyle}
+        className=" text-center py-20 text-3xl font-bold"
+      >
+        <p>Login Page</p>
+      </section>
+      <div className="login">
+        <p>Work on Progress</p>
+        <p>Stay stuned!!</p>
+        <p>Visit the other features</p>
+      </div>
     </div>
   );
 };
